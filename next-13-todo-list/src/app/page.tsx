4a8b11512e3c56c950b11ed19a35fba2d4e7857a
Jose Torres <vidojam@gmail.com>
2023-08-13 15:32:20 -0400

    first commit

---
+++ next-13-todo-list/src/app/page.tsx
@@ -0,0 +1,27 @@
+import { prisma } from "@/db"
+import Link from "next/link"
+
+export default async function Home () {
+
+  const todos = await prisma.todo.findMany()
+  return (
+    <>
+    <header>
+      <h1 className="text-2xl">Todos</h1>
+      <Link 
+      className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+      hover:bg-slate-700 focus-within:bg-slate-700
+      outline-none"
+      href="/new"
+      >
+        New
+      </Link>
+    </header>
+    <ul className="pl-4">
+      {todos.map(todo => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul> 
+    </>
+  )
+}
