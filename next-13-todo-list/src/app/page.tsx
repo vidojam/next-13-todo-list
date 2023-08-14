f948b944a12c902dbeff32931f345763506aab97
Jose Torres <vidojam@gmail.com>
2023-08-14 12:37:32 -0400

    course complete

--- next-13-todo-list/src/app/page.tsx
+++ next-13-todo-list/src/app/page.tsx
@@ -1,27 +1,36 @@
+import { TodoItem } from "@/components/TodoItem"
 import { prisma } from "@/db"
 import Link from "next/link"
 
-export default async function Home () {
+function getTodos() {
+  return prisma.todo.findMany()
+}
 
-  const todos = await prisma.todo.findMany()
+async function toggleTodo(id: string, complete: boolean) {
+  "use server"
+
+  await prisma.todo.update({ where: { id }, data: { complete } })
+}
+
+export default async function Home() {
+  const todos = await getTodos()
+
   return (
     <>
-    <header>
-      <h1 className="text-2xl">Todos</h1>
-      <Link 
-      className="border border-slate-300 text-slate-300 px-2 py-1 rounded
-      hover:bg-slate-700 focus-within:bg-slate-700
-      outline-none"
-      href="/new"
-      >
-        New
-      </Link>
-    </header>
-    <ul className="pl-4">
-      {todos.map(todo => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul> 
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+        ))}
+      </ul>
     </>
   )
 }
