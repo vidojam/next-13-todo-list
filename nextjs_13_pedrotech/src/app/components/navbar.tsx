12e994627d75e278369e3c04dc7f17c19d5eba60
Jose Torres <vidojam@gmail.com>
2023-08-16 12:02:02 -0400

    first commit

---
+++ nextjs_13_pedrotech/src/app/components/navbar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+
+export default function Navbar() {
+  return (
+  <div>
+    <Link href="/home"> Home</Link>
+    <Link href="/about"> About</Link>
+    <Link href="/us"> About Us</Link>
+    <Link href="/someone"> About Someone Else</Link>
+    </div>
+  );
+}
